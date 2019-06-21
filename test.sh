a2c87241a6fef6960d31ce8c949fc9eecdc41103
Benjamin WAN NAN WO <bwan-nan@student.42.fr>
2019-06-21 08:31:49 +0200

    Minor modif on test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 ## 2nd argument corresponds to the type of test you want the generator to do
 ## To read the generator manual: type `./generator --help`
 
-if [ "$1" != "" ] && [ "$2" != "" ] ; then
+if [ "$1" != "" ] && [ "$2" != "" ] && [ -f "generator" ] && [ -f "lem_in" ] ; then
 	total=$1
 	for ((i=1; i<=$total; i++))
 	do
@@ -10,13 +10,22 @@ if [ "$1" != "" ] && [ "$2" != "" ] ; then
 		./lem_in < test.txt > output.txt
 		rounds=`cat output.txt`
 		expected=`tail -n 1 test.txt | grep -Eo "\d+"`
-		printf 'Test %-5d: Expected: %5d | Output: %5d\n' $i $expected $rounds
 		deviation=$((rounds-expected))
-		percentage=$((deviation/expected))
+		echo "scale=2; $deviation*100/$expected" | bc > percentage.txt
+		percentage=`cat percentage.txt`
+		printf 'Test %-5d: Expected: %5d | Output: %5d  (diff: %.0f%%)\n'\
+		$i $expected $rounds $percentage
 		sum=$((sum+deviation))
-		sump=$((sump+percentage))
+		sump=$sump+$percentage
 	done
-	echo "Average deviation: $((sum/total)) rounds($((sump/total))%)"
+
+	if [[ $((sum/total)) < 2 ]] ; then
+		c=''
+	else
+		c='s'
+	fi
+	echo "\nAverage deviation: $((sum/total)) round$c"
 	rm test.txt
 	rm output.txt
+	rm percentage.txt
 fi
